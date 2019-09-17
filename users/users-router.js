9fac2bb6a1ea7f4bcd59abb323bd53108450e5d9
Shawn McManus <xsvfan76@gmail.com>
2019-09-17 16:06:00 -0600

    finished session build out for mvp

--- users/users-router.js
+++ users/users-router.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const Users = require('./users-model.js');
 const restricted = require('../users-helpers/users-helpers.js');
+const SessionMW = require('../auth/session-middleware.js');
 
 router.get('/', (req, res) => {
   res.send('Its working in the users router');
@@ -30,6 +31,7 @@ router.post('/login', (req, res) => {
     .then(user => {
       // check password
       if (user && bcrypt.compareSync(password, user.password)) {
+        req.session.user = user;
         res.status(200).json({ message: `Welcome ${user.username}!` });
       } else {
         res.status(401).json({ message: 'You cannot pass!' });
@@ -40,12 +42,28 @@ router.post('/login', (req, res) => {
     });
 });
 
-router.get('/users', restricted, (req, res) => {
+router.get('/users', SessionMW, (req, res) => {
   Users.find()
     .then(users => {
       res.json(users);
     })
     .catch(err => res.send(err));
+});
+
+router.get('/logout', (req, res) => {
+  if (req.session) {
+    req.session.destroy(error => {
+      if (error) {
+        res
+          .status(500)
+          .json({ message: 'you can checkout anytime but cant leave' });
+      } else {
+        res.status(200).json({ message: 'bye' });
+      }
+    });
+  } else {
+    res.status(200).json({ message: 'already logged out' });
+  }
 });
 
 module.exports = router;
